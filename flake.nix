7add82fbbd25765338005c11c1b584af40e0fc6a
Nick Girardo <nickgirardo@gmail.com>
2024-02-14 18:48:40 -0500

    Trone version bump

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     haskellNix.url = "github:input-output-hk/haskell.nix";
     flake-utils.url = "github:numtide/flake-utils";
     resume.url = "github:nickgirardo/resume/6c15624737a6728cef98ccda8ceaf8efd951cab7";
-    trone.url = "github:nickgirardo/trone/dfc7629ee86be4bed705f17b92d028abbf9c2fb4";
+    trone.url = "github:nickgirardo/trone/5da08acce64344a4e04b287caf29b7b34ddbb109";
     sudoku.url = "github:nickgirardo/sudoku/eb40b3464d884c2b64c340fc1052577c06916967";
     ld48-42.url = "github:nickgirardo/ld48-42/ff194c48a9127712651b4c25e6bda3a8d91873e8";
     grid-tetris.url = "github:nickgirardo/grid-tetris/9547afb3eecaf939bbb93685a48c583238cf5367";
